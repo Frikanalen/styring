cb45047c2aaa6504ba82bf1d8d0726e7dcc373a3
Tore Sinding Bekkedal <toresbe@protonmail.com>
2022-11-18 13:01:35 +0100

    Fix incorrect input index for RX1

--- src/AtemControl.tsx
+++ src/AtemControl.tsx
@@ -4,7 +4,7 @@ export const ATEM_INPUTS: MixEffectsBusInput[] = [
   { index: 2, name: "TX1" },
   { index: 3, name: "TX2" },
   { index: 1, name: "TX3" },
-  { index: 4, name: "RX1" },
+  { index: 5, name: "RX1" },
   { index: 3010, name: "Still 1" },
   { index: 1000, name: "Color bars" },
 ];
